886934ef168a721e5d1ae98a8064419ce7f22209
start.vaadin.com <no-reply@vaadin.com>
2022-09-05 13:30:48 +0000

    Generated project

---
+++ src/main/java/de/willy72/spdpg/views/impressum/ImpressumView.java
@@ -0,0 +1,31 @@
+package de.willy72.spdpg.views.impressum;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import de.willy72.spdpg.views.MainLayout;
+
+@PageTitle("Impressum")
+@Route(value = "impressum", layout = MainLayout.class)
+public class ImpressumView extends VerticalLayout {
+
+    public ImpressumView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
